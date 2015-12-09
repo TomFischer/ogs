c94b02340246b083c3270af0a317b46cdce2af55
Dmitri Naumov <dmitri.naumov@ufz.de>
2015-12-09 18:13:17 +0100

    [MeL] Shorten lines using brace initialization.

--- MeshLib/MeshInformation.cpp
+++ MeshLib/MeshInformation.cpp
@@ -25,14 +25,16 @@ const std::pair<int, int> MeshInformation::getValueBounds(const MeshLib::Mesh &m
 		= mesh.getProperties().getPropertyVector<int>("MaterialIDs");
 	if (!materialIds) {
 		INFO("Mesh does not contain a property \"MaterialIDs\".");
-		return std::make_pair(std::numeric_limits<int>::max(),std::numeric_limits<int>::max());
+		return {std::numeric_limits<int>::max(),
+		        std::numeric_limits<int>::max()};
 	}
 	if (materialIds->empty()) {
 		INFO("Mesh does not contain values for the property \"MaterialIDs\".");
-		return std::make_pair(std::numeric_limits<int>::max(),std::numeric_limits<int>::max());
+		return {std::numeric_limits<int>::max(),
+		        std::numeric_limits<int>::max()};
 	}
 	auto mat_bounds = std::minmax_element(materialIds->cbegin(), materialIds->cend());
-	return std::make_pair(*(mat_bounds.first), *(mat_bounds.second));
+	return {*(mat_bounds.first), *(mat_bounds.second)};
 }
 
 const GeoLib::AABB<MeshLib::Node> MeshInformation::getBoundingBox(const MeshLib::Mesh &mesh)
