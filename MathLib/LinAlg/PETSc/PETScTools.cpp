40f05b016211105e5702753e41c71c9615ff5fa3
Thomas Fischer <thomas.fischer@ufz.de>
2018-01-24 10:54:32 +0100

    [MaL] PETScTools: applyKnownSolutions: Use MatZeroRowsColumns.

--- MathLib/LinAlg/PETSc/PETScTools.cpp
+++ MathLib/LinAlg/PETSc/PETScTools.cpp
@@ -15,6 +15,7 @@
 */
 
 #include "PETScTools.h"
+#include "PETScVector.h"
 
 namespace MathLib
 {
@@ -23,18 +24,39 @@ void applyKnownSolution(PETScMatrix& A, PETScVector& b, PETScVector& x,
                         const std::vector<PetscScalar>& vec_knownX_x)
 {
     A.finalizeAssembly();
-
-    A.setRowsColumnsZero(vec_knownX_id);
-    A.finalizeAssembly();
-
     x.finalizeAssembly();
-    b.finalizeAssembly();
     if (vec_knownX_id.size() > 0)
     {
         x.set(vec_knownX_id, vec_knownX_x);
-        b.set(vec_knownX_id, vec_knownX_x);
+    }
+    x.finalizeAssembly();
+
+    const PetscScalar one = 1.0;
+    const PetscInt nrows = static_cast<PetscInt>(vec_knownX_id.size());
+
+    PETSc_Mat & mat(A.getRawMatrix());
+
+    // Each process will only zero its own rows.
+    // This avoids all reductions in the zero row routines
+    // and thus improves performance for very large process counts.
+    // See PETSc doc about MAT_NO_OFF_PROC_ZERO_ROWS.
+    // MatSetOption(mat, MAT_NO_OFF_PROC_ZERO_ROWS, PETSC_TRUE);
+
+    // Keep the non-zero pattern for the assignment operator.
+    MatSetOption(mat, MAT_KEEP_NONZERO_PATTERN, PETSC_TRUE);
+
+    Vec& solution(x.getRawVector());
+    Vec& rhs(b.getRawVector());
+    if (nrows > 0)
+    {
+        MatZeroRowsColumns(mat, nrows, vec_knownX_id.data(), one, solution, rhs);
+    }
+    else
+    {
+        MatZeroRowsColumns(mat, 0, PETSC_NULL, one, solution, rhs);
     }
 
+    A.finalizeAssembly();
     x.finalizeAssembly();
     b.finalizeAssembly();
 }
