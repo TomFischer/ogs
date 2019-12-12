69e4fbe110c9e22f4458b63f0e5cc2e56c3117c6
Thomas Fischer <thomas.fischer@ufz.de>
2019-12-12 15:32:13 +0100

    fixup! [T/HT] Update constraint_bc test.

--- ProcessLib/HT/Tests.cmake
+++ ProcessLib/HT/Tests.cmake
@@ -365,7 +365,7 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
-    GLOB constraint_bc_1e3_pcs_0_ts_*.vtu T T 1e-14 1e-13
+    GLOB constraint_bc_1e3_pcs_0_ts_*.vtu T T 1e-11 1e-11
     GLOB constraint_bc_1e3_pcs_0_ts_*.vtu p p 1e-15 1e-14
 )
 
